fb22196eac83f0cbc9b06860e91d3559d80361e9
Szabo <szabo.48@phy-nc188115.asc.ohio-state.edu>
2020-01-06 12:37:54 -0500

    Added input args

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import pandas as pd
 NAME=sys.argv[1];
 init_spin=sys.argv[2];
 init_cavity=sys.argv[3];
-boundary=sys.argv[4];
+init_cavity_num=sys.argv[4];
+boundary=sys.argv[5];
 
 
 df_columns = ["L", "coupling", "omega", "Jzz", "Jx", "Boundary", "Times",
@@ -13,23 +14,45 @@ df_columns = ["L", "coupling", "omega", "Jzz", "Jx", "Boundary", "Times",
 df = pd.DataFrame(columns=df_columns);
 
 
-field_vals = np.linspace(0, 3, 40);
-coupling_vals = np.linspace(0, 1.2, 10);
-omega_vals = (0.5, 0.8, 1.2, 5.0);
+field_vals = np.linspace(0, 3, 2);
+coupling_vals = np.linspace(0, 1.2, 2);
+#omega_vals = (0.5, 0.8, 1.2, 5.0);
 
-for omega in omega_vals:
-    for field in field_vals:
-        for coupling in coupling_vals:
+if boundary == "O":
+    BC = False;
+elif boundary = "P":
+    BC = True;
+else:
+    BC = True;
 
-            t, AC_ent, Sent, Obs_t, obs_pht_ray, g2_0, pairwise_concurrence = spin_photon_Nsite_DM(8, omega * coupling,
-                                            decouple=0,Omega=omega,Nph_tot=50,photon_state=0,t_steps=2000,
-                                            obs_photon=2000,t_max=400.0,state='ferro',vect='z',
-                                            J_zz=1.0,J_x=field,return_state_DM=False,periodic=True, init_state=init_state);
+if init_cavity == "Fock":
+    CH = False;
+elif boundary = "CH":
+    CH = True;
+else:
+    CH = False;
 
-            df = df.append({"L": 8, "coupling": omega * coupling,"omega": omega,"Jzz":1.0,
-                            "Jx":x_field,"Boundary":boundary,"Times":t,"P-S Ent":AC_ent,
-                            "S-S Ent":Sent,"Observables":Obs,"P_DM":obs_pht_ray,
-                            "G2_0_T":g2_0,"PW_CONC":pairwise_concurrence}, ignore_index=True);
+#for omega in omega_vals:
+omega = 0.5;
+for field in field_vals:
+    for coupling in coupling_vals:
+
+        if init_spin == "UNIFORM":
+            init_state=None;
+        elif init_spin=="EQ":
+            init_state=(1.0, field);
+        elif init_spin=="Quench":
+            init_state=(1.0, 5.0);
+
+        t, AC_ent, Sent, Obs_t, obs_pht_ray, g2_0, pairwise_concurrence = spin_photon_Nsite_DM(4, omega * coupling,
+                                        decouple=0,Omega=omega,Nph_tot=50,coherent=CH,photon_state=init_cavity_num,t_steps=2000,
+                                        obs_photon=2000,t_max=400.0,state='ferro',vect='z',
+                                        J_zz=1.0,J_x=field,return_state_DM=False,periodic=BC, init_state=init_state);
+
+        df = df.append({"L": 8, "coupling": omega * coupling,"omega": omega,"Jzz":1.0,
+                        "Jx":x_field,"Boundary":boundary,"Times":t,"P-S Ent":AC_ent,
+                        "S-S Ent":Sent,"Observables":Obs,"P_DM":obs_pht_ray,
+                        "G2_0_T":g2_0,"PW_CONC":pairwise_concurrence}, ignore_index=True);
 
 
 df.to_pickle("/Users/szabo.48/Desktop/"+ NAME +".pickle");
